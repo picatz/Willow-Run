43e65f1762dc910c924bd0a3d58105d7467c039a
Kent Gruber <kgruber1@emich.edu>
2016-11-24 21:17:50 -0500

    Update scanner spec

--- spec/willow_run_scanner_spec.rb
+++ spec/willow_run_scanner_spec.rb
@@ -4,21 +4,24 @@ describe WillowRun::Scanner do
   scanner = WillowRun::Scanner.new
   
   context "without a provided ssid" do
-    data = scanner.scan.data
-
     it "can scan without a provided ssid" do
       expect(scanner.scan).not_to be false
     end
 
-    it "can work with access point data" do
-      expect(data).not_to be false
+    it "can work with scanned access point data" do
+      expect(scanner.scan.data[0].data).not_to be false
     end
   end
-  
+
   context "with a provided ssid" do
+    known_ssid = scanner.scan.data[0].data.ssid_str
+    
     it "can scan with a known ssid" do
-      known_ssid = scanner.scan.data[0].data.ssid_str
       expect(scanner.scan(known_ssid)).not_to be false
     end  
+    
+    it "can work with known scanned access point data" do
+      expect(scanner.scan(known_ssid).data).not_to be false
+    end
   end
 end
