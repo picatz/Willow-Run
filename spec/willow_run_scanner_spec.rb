08eb97a65c7b102ad4b349bdb4241d9ed17c83a3
Kent Gruber <kgruber1@emich.edu>
2016-11-24 21:07:28 -0500

    Update scanner spec

--- spec/willow_run_scanner_spec.rb
+++ spec/willow_run_scanner_spec.rb
@@ -1,16 +1,24 @@
 require "spec_helper"
 
 describe WillowRun::Scanner do
-  it "can scan without a provided ssid" do
-    expect(WillowRun::Scanner.new.scan).not_to be false
-  end
+  scanner = WillowRun::Scanner.new
+  
+  context "without a provided ssid" do
+    data = scanner.scan.data
 
+    it "can scan without a provided ssid" do
+      expect(scanner.scan).not_to be false
+    end
+
+    it "can work with access point data" do
+      expect(data).not_to be false
+    end
+  end
+  
   context "with a provided ssid" do
-    it "can scan with an known ssid" do
-      scanner = WillowRun::Scanner.new
+    it "can scan with a known ssid" do
       known_ssid = scanner.scan.data[0].data.ssid_str
-      expect(WillowRun::Scanner.new.scan(known_ssid)).not_to be false
+      expect(scanner.scan(known_ssid)).not_to be false
     end  
   end
-
 end
